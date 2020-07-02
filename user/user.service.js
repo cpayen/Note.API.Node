567e396037c269af5f14ca74930c833b64277550
Camille Payen <camille.payen.pro@gmail.com>
2020-07-02 16:37:20 +0200

    Add user authentication

--- user/user.service.js
+++ user/user.service.js
@@ -1,19 +1,16 @@
 const config = require('config.json');
 const jwt = require('jsonwebtoken');
-
-// users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   authenticate
 };
 
 async function authenticate({ username, password }) {
-  const user = users.find(u => u.username === username && u.password === password);
-
+  const user = await findUser(username, password);
   if (!user) throw 'Username or password is incorrect';
 
-  // create a jwt token that is valid for 7 days
   const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
 
   return {
@@ -23,6 +20,22 @@ async function authenticate({ username, password }) {
 }
 
 // helper functions
+
+async function findUser(username, password) {
+  const userFilePath = path.resolve(config.dataPath, 'users', `${username}.json`);
+  let user = false;
+
+  await fs.promises.readFile(userFilePath) 
+    .then((result) => { 
+      const jsonUser = JSON.parse(result);
+      if(jsonUser.password === password) {
+        user = jsonUser;
+      }
+    }) 
+    .catch();
+  
+  return user;
+}
 
 function omitPassword(user) {
   const { password, ...userWithoutPassword } = user;
