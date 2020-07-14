53070f50660aa34da2eaf118867ad0526ad31737
Camille Payen <camille.payen.pro@gmail.com>
2020-07-14 12:28:47 +0200

    Fix user authentication

--- app/user/user.service.js
+++ app/user/user.service.js
@@ -1,7 +1,5 @@
 const config = require('../../config.json');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const path = require('path');
 const db = require('../../helpers/db');
 
 module.exports = {
@@ -9,7 +7,7 @@ module.exports = {
 };
 
 async function authenticate({ username, password }) {
-  const user = await findUser(username, password);
+  const user = await db.authUser(username, password);
   if (!user) throw 'Username or password is incorrect';
 
   const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d', algorithm: 'HS256' });
@@ -18,24 +16,6 @@ async function authenticate({ username, password }) {
     ...omitPassword(user),
     token
   };
-}
-
-// helper functions
-
-async function findUser(username, password) {
-  const userFilePath = path.resolve(db.getUsersPath(), `${username}.json`);
-  let user = false;
-
-  await fs.promises.readFile(userFilePath) 
-    .then((result) => { 
-      const jsonUser = JSON.parse(result);
-      if(jsonUser.password === password) {
-        user = jsonUser;
-      }
-    }) 
-    .catch();
-  
-  return user;
 }
 
 function omitPassword(user) {
