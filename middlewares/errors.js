56ee963cdb59603a306e94826ddf42acae1cf217
Camille Payen <camille.payen.pro@gmail.com>
2020-07-20 18:23:11 +0200

    Add logger

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,25 +1,28 @@
+const logger = require('../helpers/logger');
+
 module.exports = errorHandler;
 
 function errorHandler(err, req, res, next) {
 
-  // debug
-  console.error(err);
-
   // custom application error
   if (typeof (err) === 'string') {
+    logger.info(err);
     return res.status(400).json({ message: err });
   }
 
   // jwt authentication error
   if (err.name === 'UnauthorizedError') {
+    logger.info(err);
     return res.status(401).json({ message: 'Invalid Token' });
   }
 
   // resource not fond error
   if (err.name === 'ResourceNotFoundError') {
+    logger.info(err);
     return res.status(404).json({ message: 'Resource not found' });
   }
 
   // default to 500 server error
+  logger.error(err);
   return res.status(500).json({ message: 'An error occurred' });
 }
