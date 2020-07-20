002d8ad001a9982e261b77bc6efcd5ca37a97791
Camille Payen <camille.payen.pro@gmail.com>
2020-07-20 22:00:53 +0200

    Enhance error display in logs

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,42 +1,42 @@
 var winston = require('winston');
 const { format } = require('logform');
 
-const alignedWithTime = format.combine(
+const fileFormat = format.combine(
   format.timestamp(),
   format.align(),
-  format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  format.errors({ stack: true }),
+  format.printf(info => {
+    if(info.stack) {
+      return `${info.timestamp} ${info.level}: ${info.message} \n${info.stack}`;
+    }
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
 );
 
-const alignedWithColorsAndTime = format.combine(
+const consoleFormat = format.combine(
   format.colorize(),
   format.timestamp(),
-  format.align(),
-  format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  format.errors({ stack: true }),
+  format.printf(info => {
+    if(info.stack) {
+      return `${info.timestamp} ${info.level}: ${info.message} \n${info.stack}`;
+    }
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
 );
 
 const logger = winston.createLogger({
   level: 'info',
-  //format: winston.format.simple(),
-  format: alignedWithTime,
-  //defaultMeta: { service: 'user-service' },
+  format: fileFormat,
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
-    //format: winston.format.simple(),
-    format: alignedWithColorsAndTime,
+    format: consoleFormat,
   }));
 }
 
